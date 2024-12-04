ed13d9d0d7b7b4d51172b63aaf1e5604f78015b7
Esral Desta <esraldesta@gmail.com>
2024-12-04 10:23:51 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(parseInt(process.env.PORT) || 3000)
+  await app.listen(parseInt(8080)
   app.useGlobalFilters(new HttpExceptionFilter());
 }
 bootstrap();
