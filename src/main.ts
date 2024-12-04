2f051a9a84604b29582cff58cf360fb024c14f98
Esral Desta <esraldesta@gmail.com>
2024-12-04 11:48:31 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './execption.filter';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen("8080")
+  await app.listen(process.env.PORT ?? 8000);
   app.useGlobalFilters(new HttpExceptionFilter());
 }
 bootstrap();
